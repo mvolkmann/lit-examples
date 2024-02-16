bf053172c60a573f01e0c5cc619abf1c043df467
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-16 13:29:49 -0600

    progress

--- lit-with-vite/src/state-changes.ts
+++ lit-with-vite/src/state-changes.ts
@@ -5,6 +5,7 @@ import {customElement, property, state} from 'lit/decorators.js';
 export class StateChanges extends LitElement {
   @property() p = '';
   @state() s = 'initial';
+  v = 'non-reactive';
 
   changeP() {
     this.p = 'changed';
@@ -12,6 +13,14 @@ export class StateChanges extends LitElement {
 
   changeS() {
     this.s = 'changed';
+  }
+
+  changeV() {
+    this.v = 'changed';
+    console.log('state-changes.ts changeV: v =', this.v);
+    // v is not a reactive property,
+    // so the change won't trigger a call to render.
+    this.requestUpdate();
   }
 
   render() {
@@ -23,6 +32,10 @@ export class StateChanges extends LitElement {
       <div>
         s: ${this.s}
         <button @click=${this.changeS}>Change</button>
+      </div>
+      <div>
+        v: ${this.v}
+        <button @click=${this.changeV}>Change</button>
       </div>
     `;
   }
