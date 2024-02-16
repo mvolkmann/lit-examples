20025073a38b3640d05b3c14cdf053769e23b659
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-16 12:04:39 -0600

    progress

--- lit-with-vite/src/lifecycle-demo.ts
+++ lit-with-vite/src/lifecycle-demo.ts
@@ -14,7 +14,7 @@ export class LifecycleDemo extends LitElement {
     console.log('constructor entered');
   }
 
-  attributeChangedCallback(
+  override attributeChangedCallback(
     name: string,
     oldValue: string | null,
     newValue: string | null
@@ -23,12 +23,12 @@ export class LifecycleDemo extends LitElement {
     super.attributeChangedCallback(name, oldValue, newValue);
   }
 
-  connectedCallback(): void {
+  override connectedCallback(): void {
     super.connectedCallback();
     console.log('connectedCallback entered');
   }
 
-  disconnectedCallback(): void {
+  override disconnectedCallback(): void {
     super.disconnectedCallback();
     console.log('disconnectedCallback entered');
   }
@@ -36,7 +36,7 @@ export class LifecycleDemo extends LitElement {
   render() {
     return html`
       <div>
-        p: ${this.text}
+        text: ${this.text}
         <button @click=${this.changeText}>Change</button>
       </div>
     `;
