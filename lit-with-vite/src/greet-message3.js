915fd8ccb00b801c6d425f2d75233a61fe52119c
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-17 14:47:19 -0600

    progress

--- lit-with-vite/src/greet-message3.js
+++ lit-with-vite/src/greet-message3.js
@@ -5,17 +5,30 @@ export class GreetMessage3 extends HTMLElement {
     const name = this.getAttribute('name');
     if (!name) throw new Error('name is a required attribute');
 
+    /*
     const div = document.createElement('div');
     div.textContent = `Hello, ${name}!`;
     div.style.color = 'purple';
+    */
 
     /*
-    const shadow = this.attachShadow({mode: 'open'});
-    shadow.appendChild(div);
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(div);
     */
     // Using shadow DOM is not required.  We can replace
     // the previous two lines with the following.
-    this.appendChild(div);
+    // this.appendChild(div);
+
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.innerHTML = `
+      <div style="color: purple;">Hello, ${name}!</div>
+    `;
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log('attributeChangedCallback: name =', name);
+    console.log('attributeChangedCallback: oldValue =', oldValue);
+    console.log('attributeChangedCallback: newValue =', newValue);
   }
 }
 customElements.define('greet-message3', GreetMessage3);
