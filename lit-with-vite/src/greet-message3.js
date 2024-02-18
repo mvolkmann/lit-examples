17d1ef29d25d28764e9262ec82d00f1ce936ea78
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-17 19:23:48 -0600

    progress

--- lit-with-vite/src/greet-message3.js
+++ lit-with-vite/src/greet-message3.js
@@ -1,34 +1,53 @@
-export class GreetMessage3 extends HTMLElement {
+export class GreetMessage extends HTMLElement {
   constructor() {
     super();
 
     const name = this.getAttribute('name');
     if (!name) throw new Error('name is a required attribute');
 
+    // Approach #1
+    // Using shadow DOM is not required.
     /*
     const div = document.createElement('div');
     div.textContent = `Hello, ${name}!`;
     div.style.color = 'purple';
+    this.appendChild(div);
     */
 
+    // Approach #2
     /*
+    const div = document.createElement('div');
+    div.textContent = `Hello, ${name}!`;
+    div.style.color = 'purple';
+
+    // The "mode" option is required and the recommended value is "open".
+    // When set to "open", `this.shadowRoot` is set.
+    // When set to "closed", it is not.
     this.attachShadow({mode: 'open'});
     this.shadowRoot.appendChild(div);
     */
-    // Using shadow DOM is not required.  We can replace
-    // the previous two lines with the following.
-    // this.appendChild(div);
 
+    // Approach #3
     this.attachShadow({mode: 'open'});
-    this.shadowRoot.innerHTML = `
-      <div style="color: purple;">Hello, ${name}!</div>
-    `;
+    this.render();
+  }
+
+  static get observedAttributes() {
+    return ['name'];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    console.log('attributeChangedCallback: name =', name);
-    console.log('attributeChangedCallback: oldValue =', oldValue);
-    console.log('attributeChangedCallback: newValue =', newValue);
+    if (name === 'name') this.render();
+  }
+
+  render() {
+    const name = this.getAttribute('name');
+    this.shadowRoot.innerHTML = `
+      <style>
+        div { color: var(--greet-message-color, purple); }
+      </style>
+      <div>Hello, ${name}!</div>
+    `;
   }
 }
-customElements.define('greet-message3', GreetMessage3);
+customElements.define('greet-message3', GreetMessage);
