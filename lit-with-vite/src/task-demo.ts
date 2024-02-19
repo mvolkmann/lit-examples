aeccd66fa69d8d91fbcad341d03cfa83b1e9013d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-19 14:06:09 -0600

    more Lit

--- lit-with-vite/src/task-demo.ts
+++ lit-with-vite/src/task-demo.ts
@@ -15,8 +15,16 @@ async function getTodo(id: number): Promise<Todo> {
   // Simulate a long-running query.
   return new Promise((resolve, reject) => {
     setTimeout(async () => {
-      const res = await fetch(URL_PREFIX + id);
-      resolve(await res.json());
+      try {
+        const res = await fetch(URL_PREFIX + id);
+        if (res.ok) {
+          resolve(await res.json());
+        } else {
+          reject(`bad status ${res.status}`);
+        }
+      } catch (error) {
+        reject(error);
+      }
     }, 1000);
   });
 }
@@ -38,18 +46,20 @@ export class TaskDemo extends LitElement {
   }
 
   override render() {
+    // What this renders is based on the whether the task
+    // is pending, complete, or has experienced an error.
     const taskDisplay = this.task.render({
       pending: () => html`<img alt="spinner" src="/spinner.gif" />`,
       complete: todo => html`
         <h2>${todo.title}</h2>
-        <div>${todo.completed ? 'complete' : 'pending'}</div>
+        <div>status: ${todo.completed ? 'complete' : 'pending'}</div>
       `,
-      error: error => html`<div>Error: ${error}</div>`
+      error: error => html`<div class="error">${error}</div>`
     });
 
     return html`
-      <label
-        >Todo Id
+      <label>
+        Todo ID
         <input
           type="number"
           @input=${this.changeTodoId}
@@ -62,9 +72,13 @@ export class TaskDemo extends LitElement {
 
   static styles = css`
     :host {
-      border: 1px dashed red;
+      border: 1px solid gray;
       padding: 1rem;
       width: 20rem;
+    }
+
+    .error {
+      color: red;
     }
 
     h2 {
