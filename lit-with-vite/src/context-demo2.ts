d2cc8cb1bdeb24c92988970bdb9be9acd6e751e7
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-20 13:40:10 -0600

    progress

---
+++ lit-with-vite/src/context-demo2.ts
@@ -0,0 +1,46 @@
+import {html, LitElement} from 'lit';
+import {consume, createContext, provide} from '@lit/context';
+import {customElement} from 'lit/decorators.js';
+import {dogContext, type Dog} from './dog-context.js';
+
+type Data = {count: number};
+export const dataContext = createContext<Data>('data');
+
+@customElement('my-parent')
+export class MyParent extends LitElement {
+  @provide({context: dataContext}) data: Data = {count: 0};
+
+  increment() {
+    // this.data.count++; // doesn't work; must replace context object
+    this.data = {count: this.data.count + 1};
+  }
+
+  override render() {
+    return html`
+      <div @increment=${this.increment}>
+        <button @click=${this.increment}>Parent Increment</button>
+        <my-child></my-child>
+        <my-child></my-child>
+      </div>
+    `;
+  }
+}
+
+@customElement('my-child')
+export class MyChild extends LitElement {
+  @consume({context: dataContext, subscribe: true}) data?: Data;
+
+  increment() {
+    const {host} = this.shadowRoot;
+    host.dispatchEvent(new CustomEvent('increment', {bubbles: true}));
+  }
+
+  override render() {
+    return html`
+      <div>
+        <div>count = ${this.data.count}</div>
+        <button @click=${this.increment}>Child Increment</button>
+      </div>
+    `;
+  }
+}
