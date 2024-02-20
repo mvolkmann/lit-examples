a87defdfd992bc61220ad21abd5b2102c4549738
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-20 11:11:35 -0600

    progress

---
+++ lit-with-vite/src/vanilla-wc.js
@@ -0,0 +1,42 @@
+export class VanillaWC extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.render();
+  }
+
+  render() {
+    const root = this.shadowRoot;
+    root.innerHTML = `
+      <div>
+        <h2>Vanilla Web Component</h2>
+        <nav><slot name="nav"></slot></nav>
+        <div part="header">header</div>
+        <p><slot></slot></p>
+      </div>
+    `;
+
+    const sheet = new CSSStyleSheet();
+    sheet.replaceSync(`
+      :host {
+        display: inline-block;
+
+        border: 1px solid blue;
+        padding: 1rem;
+        width: 30%;
+      }
+      slot[name="nav"]::slotted(*) {
+        color: red;
+      }
+      ::slotted(*) {
+        color: green;
+        font-style: italic;
+      }
+    `);
+    // The ::slotted pseudo-element only works
+    // in CSS specified inside a shadow DOM.
+    // It applies to actual elements in slots, not text nodes.
+    root.adoptedStyleSheets = [sheet];
+  }
+}
+customElements.define('vanilla-wc', VanillaWC);
