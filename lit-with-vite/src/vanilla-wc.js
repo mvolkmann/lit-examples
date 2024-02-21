c2518fe1e95c2b6758c1f85936648e8b95818f24
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-20 20:17:46 -0600

    progress

--- lit-with-vite/src/vanilla-wc.js
+++ lit-with-vite/src/vanilla-wc.js
@@ -5,6 +5,33 @@ export class VanillaWC extends HTMLElement {
     const root = this.shadowRoot;
 
     root.innerHTML = `
+      <style>
+        /* This targets the shadow host. */
+        :host {
+          display: inline-block;
+
+          border: 1px solid blue;
+          padding: 1rem;
+          width: 30%;
+        }
+
+        /* This targets any top-level child placed in the slot named "nav". */
+        slot[name="nav"]::slotted(*) {
+          border-bottom: 1px solid blue;
+        }
+
+        /* This could be used in place of the previous rule.
+        nav {
+          border-bottom: 1px solid blue;
+        } */
+
+        /* This targets any top-level p elements placed in any slot.
+          ::slotted only supports single-element selectors. */
+        ::slotted(p) {
+          color: green;
+          font-style: italic;
+        }
+      </style>
       <div>
         <h2>Vanilla Web Component</h2>
         <nav><slot name="nav"></slot></nav>
@@ -12,39 +39,6 @@ export class VanillaWC extends HTMLElement {
         <p><slot></slot></p>
       </div>
     `;
-
-    const sheet = new CSSStyleSheet();
-    sheet.replaceSync(`
-      /* This targets the shadow host. */
-      :host {
-        display: inline-block;
-
-        border: 1px solid blue;
-        padding: 1rem;
-        width: 30%;
-      }
-
-      /* This targets any top-level child placed in the slot named "nav". */
-      slot[name="nav"]::slotted(*) {
-        border-bottom: 1px solid blue;
-      }
-
-      /* This could be used in place of the previous rule.
-      nav {
-        border-bottom: 1px solid blue;
-      } */
-
-      /* This targets any top-level p elements placed in any slot.
-         ::slotted only supports single-element selectors. */
-      ::slotted(p) {
-        color: green;
-        font-style: italic;
-      }
-    `);
-    // The ::slotted pseudo-element only works
-    // in CSS specified inside a shadow DOM.
-    // It applies to actual elements in slots, not text nodes.
-    root.adoptedStyleSheets = [sheet];
   }
 }
 customElements.define('vanilla-wc', VanillaWC);
