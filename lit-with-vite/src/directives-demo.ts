85efecd319fb2c522e48848f0c8a3fe4b5186284
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-25 18:31:17 -0600

    progress

--- lit-with-vite/src/directives-demo.ts
+++ lit-with-vite/src/directives-demo.ts
@@ -1,15 +1,20 @@
 import {html, LitElement} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
+import {choose} from 'lit/directives/choose.js';
+import {classMap} from 'lit/directives/class-map.js';
+import {ifDefined} from 'lit/directives/if-defined.js';
+import {join} from 'lit/directives/join.js';
 import {map} from 'lit/directives/map.js';
 import {range} from 'lit/directives/range.js';
 import {repeat} from 'lit/directives/repeat.js';
+import {styleMap} from 'lit/directives/style-map.js';
 import {when} from 'lit/directives/when.js';
 
 @customElement('directives-demo')
 export class DirectivesDemo extends LitElement {
-  @property({type: Boolean}) timeOnly;
-
-  // @property({type: Number}) count = 0;
+  @property() color?: string;
+  @property() fruit!: string;
+  @property({type: Boolean}) timeOnly = false;
 
   static fruits = [
     {id: 7, name: 'apple', color: 'red'},
@@ -18,27 +23,12 @@ export class DirectivesDemo extends LitElement {
     {id: 9, name: 'orange', color: 'orange'}
   ];
 
-  // The `when` directive is useful when content must be computed
-  // based on a condition. It takes a condition and two functions,
-  // one to compute the true content and one to compute the false content.
-  // In the case below, we could just do this:
-  // const content = this.happy ? '😊' : '😢';
-
-  // The `choose` function is similar to the `when` function,
-  // but bases the content on a non-Boolean value.
-  // It is similar to a switch statement in JavaScript.
-  // Each case computes the content to use with a function.
-
-  // The `repeat` directive is similar to the `map` function,
-  // but it takes an iterable (typically an Array),
-  // a function to compute a "key" value,
-  // and a function to compute content.
-  // The key value is used to optimize DOM updates.
-  // It is not passed to the content function.
-  // If only one function is passed, this behaves the same as `map`.
-
-  // There is always the option to use JavaScript to compute content
-  // instead of using Lit directives.
+  static fruitMap = {
+    apple: 'red',
+    banana: 'yellow',
+    blueberry: 'blue',
+    orange: 'orange'
+  };
 
   getDate() {
     return new Date().toLocaleString();
@@ -49,9 +39,30 @@ export class DirectivesDemo extends LitElement {
   }
 
   override render() {
+    const to = this.timeOnly;
+    const classes = {c1: to, c2: to, c3: to};
+    const styles = {
+      color: to ? 'purple' : 'blue',
+      fontSize: to ? '2rem' : '1rem',
+      fontWeight: to ? 'bold' : 'normal'
+    };
+
     return html`
       <section>
-        <div>${when(this.timeOnly, this.getTime, this.getDate)}</div>
+        <div class=${classMap(classes)} style=${styleMap(styles)}>
+          My favorite fruit color is
+          ${choose(
+            this.fruit,
+            [
+              // These functions can return an `html` tagged template literal.
+              ['apple', () => 'red'],
+              ['banana', () => 'yellow'],
+              ['blueberry', () => 'blue'],
+              ['orange', () => 'orange']
+            ],
+            () => `invalid fruit ${this.fruit}`
+          )}.
+        </div>
 
         <ul>
           ${map(
@@ -74,6 +85,24 @@ export class DirectivesDemo extends LitElement {
 
         <!-- This renders 10, 20, 30. -->
         ${repeat(range(3), number => html`<div>${(number + 1) * 10}</div>`)}
+
+        <!-- The style attribute is only set if
+             a value for this.color was supplied. -->
+        <div style="color: ${ifDefined(this.color)}">
+          ${when(this.timeOnly, this.getTime, this.getDate)}
+        </div>
+
+        <div
+          style="border: 1px solid gray; display: inline-block; padding: 1rem"
+        >
+          ${join(
+            map(
+              DirectivesDemo.fruits,
+              fruit => html`<div>${fruit.name}</span>`
+            ),
+            html`<hr />`
+          )}
+        </div>
       </section>
     `;
   }
